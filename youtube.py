e650b3a3d29bc748a0db3bb0b20f7b44122a0fb3
Yuri C.IV <145846514+CRazorIV@users.noreply.github.com>
2025-03-12 13:35:52 +0100

    project basis setup
    now it is time to expand it with tkinter and user inputs.

--- youtube.py
+++ youtube.py
@@ -1,12 +1,16 @@
-from pytube import YouTube
-import tkinter as tk
-from tkinter import filedialog
-
-def download_vid(file_path, url):
+from pytubefix import YouTube
+def download_vid(save_path, url):
     try:
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension='mp4')
         highest_resolution = streams.get_highest_resolution()
+        highest_resolution.download(output_path=save_path)
+        print("Video download complete!")
     except Exception as e:
         print(e)
+
+url = "https://www.youtube.com/watch?v=74WOClL8t-o"
+save_path ='C:/Users/yurik/Documents/GitHub/YT_downloader'
+
+download_vid(save_path, url)
 
